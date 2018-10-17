c377f337c3b3fdc3dca388a0e47457e354e71c83
gpietrafesavieitez <gpietrafesavieitez@nautilus31.danielcastelao.org>
2018-10-17 11:45:47 +0200

    Done

--- Boletin_3/src/boletin_3/Boletin_3.java
+++ Boletin_3/src/boletin_3/Boletin_3.java
@@ -8,10 +8,10 @@ public class Boletin_3 {
         obxConsumo1.setLitros(50);
         obxConsumo1.setPGas(1.57f);
         
-        Consumo obxConsumo2 = new Consumo(74,3.42f,87.5f,3.66f);
+        Consumo obxConsumo2 = new Consumo(74, 3.42f, 87.5f, 3.66f);
         System.out.println("O consumo medio é de: " + obxConsumo2.consumoMedio() + " litros.");
         obxConsumo2.setLitros(3);
-        System.out.println("A velocidade media é de: " + obxConsumo2.getTempo() + " km/h");
+        System.out.println("A velocidade media é de: " + obxConsumo2.getVMed() + " km/h");
     }
     
 }
